5e629af0184ddc60b4ac968347b199ed9a31a7d0
cracada <lucky@crsay.com>
2023-04-04 21:03:32 +0800

    Delete extension-scripts/safari-extensions directory

--- extension-scripts/safari-extensions/scripts/eruda.js
+++
@@ -1,6 +0,0 @@
-(function () {
-  var script = document.createElement('script');
-  script.type = 'text/javascript';
-  script.innerHTML = "var script = document.createElement('script');script.type = 'text/javascript';script.src = '//cdn.jsdelivr.net/npm/eruda';script.onload = function () { eruda.init() };document.body.appendChild(script);"
-  document.body.appendChild(script);
-})();
--- extension-scripts/safari-extensions/scripts/vconsole.js
+++
@@ -1,6 +0,0 @@
-(function () {
-  var script = document.createElement('script');
-  script.type = 'text/javascript';
-  script.innerHTML = "var script = document.createElement('script');script.type = 'text/javascript';script.src = '//cdn.jsdelivr.net/npm/vconsole';script.onload = function () {var vConsole = new VConsole(); };document.body.appendChild(script);"
-  document.body.appendChild(script);
-})();
